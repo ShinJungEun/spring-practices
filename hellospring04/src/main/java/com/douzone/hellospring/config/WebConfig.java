fca733f7b8762dc050c0f97ed985e50c18f40c63
dddgg9511 <39615281+dddgg9511@users.noreply.github.com>
2020-03-26 09:27:06 +0900

    hellospring04 WebConfig 수정

--- hellospring04/src/main/java/com/douzone/hellospring/config/WebConfig.java
+++ hellospring04/src/main/java/com/douzone/hellospring/config/WebConfig.java
@@ -15,7 +15,7 @@ public class WebConfig {
 	@Bean
 	public ViewResolver viewResolver() {
 		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-		resolver.setPrefix("/WEB-INF/views");
+		resolver.setPrefix("/WEB-INF/views/");
 		resolver.setSuffix(".jsp");
 		resolver.setExposeContextBeansAsAttributes(true);
 		return resolver;
